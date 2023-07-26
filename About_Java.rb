6e0b897f51d315c4dfba3a271a44c277c03a186d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-26 08:58:36 +0300

    Create About_Java.rb

---
+++ About_Java.rb
@@ -0,0 +1,21 @@
+Yes, Java is open source, but the situation around Javas licensing and usage has evolved over time. As of my last update in September 2021, here are some key points regarding Javas licensing and usage:
+
+OpenJDK: Java is available as an open-source implementation called OpenJDK (Java Development Kit). OpenJDK is distributed under the GNU General Public License (GPL) version 2 with the Classpath Exception. This license allows you to freely use, modify, and distribute OpenJDK without violating any copyright or legal issues.
+
+Oracle JDK vs. OpenJDK: Historically, Oracle provided the Oracle JDK distribution, which included some additional features and commercial support. Oracle JDK required a commercial license for use in certain scenarios, especially in production environments, which could lead to licensing costs.
+
+LTS Releases: Starting with Java 11 (released in September 2018), Oracle changed the distribution model for Oracle JDK. Oracle JDK became a paid subscription service for commercial use and long-term support (LTS) releases. However, the OpenJDK distribution remained free and open source.
+
+AdoptOpenJDK and Other Providers: To avoid licensing costs and ensure access to free LTS versions of Java, many developers and organizations started using alternative OpenJDK distributions like AdoptOpenJDK, Amazon Corretto, or others. These distributions offer builds based on OpenJDK with long-term support and no commercial licensing requirements.
+
+TCK Compatibility: While OpenJDK is open source, there might be legal considerations if you aim to provide a Java implementation that claims to be fully compatible with the Java Platform, Standard Edition (Java SE). Compatibility requires passing the Technology Compatibility Kit (TCK) tests, which are proprietary tests provided by Oracle. As of my last update, passing the TCK and claiming full Java SE compatibility might require a commercial license from Oracle.
+
+Consult Legal Advice: Its essential to consult with legal counsel or licensing experts for precise and up-to-date information regarding Javas usage and licensing. The landscape might have changed since my last update, and legal interpretations can be nuanced.
+
+In summary, you can freely use OpenJDK and its derivatives, such as AdoptOpenJDK, for most purposes without facing copyright strikes or legal issues from Oracle. However, if you need commercial support, additional features, or want to claim full Java SE compatibility, you should be aware of the licensing requirements associated with specific distributions and consult legal advice if necessary.
+
+
+
+
+
+  
