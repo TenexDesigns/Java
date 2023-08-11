eb9b4fd83d31bccaa0c1f01e29a2e0aa7a6e325c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-11 16:25:24 +0300

    Create Abstract_methods.c

---
+++ Basics/Methods/Abstract_methods.c
@@ -0,0 +1,71 @@
+In Java, there are methods in a superclass that are designed to be overridden by its subclasses. These methods are typically declared with the `abstract` keyword in the superclass. An abstract method is a method that has no implementation in the superclass and is meant to be implemented in its subclasses.
+
+Here's what you need to know about abstract methods and how to work with them:
+
+1. **Abstract Methods**:
+   - An abstract method is a method declared in a superclass without a body (no curly braces and no implementation).
+   - It's meant to be overridden by concrete (non-abstract) subclasses.
+   - Abstract methods provide a common interface that subclasses must implement.
+
+2. **Abstract Classes**:
+   - A class that contains one or more abstract methods must be declared as abstract using the `abstract` keyword.
+   - Abstract classes cannot be instantiated directly. They serve as templates for concrete subclasses to extend.
+
+3. **Subclass Responsibilities**:
+   - When a subclass extends an abstract class that contains abstract methods, the subclass is required to provide concrete implementations for all the abstract methods.
+
+Here's an example:
+
+```java
+abstract class Shape {
+    abstract double area(); // Abstract method
+
+    void printDescription() {
+        System.out.println("This is a shape.");
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double width, height;
+
+    Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    double area() {
+        return width * height;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        circle.printDescription();
+        System.out.println("Circle area: " + circle.area());
+
+        rectangle.printDescription();
+        System.out.println("Rectangle area: " + rectangle.area());
+    }
+}
+```
+
+In this example, the `Shape` class is abstract and contains an abstract method `area()`. The `Circle` and `Rectangle` classes extend `Shape` and provide concrete implementations for the `area()` method. The `Shape` class also has a non-abstract method `printDescription()`, which is inherited by the subclasses.
+
+Abstract methods are particularly useful for creating a common interface that multiple classes share while allowing each class to define its own implementation. They enforce a consistent structure across different subclasses and promote polymorphism.
