7180bce4cc907a5f0828b80441757c7873262409
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-14 15:40:16 +0300

    Create Free_JDK.c

---
+++ Java_and_Oracle/Free_JDK.c
@@ -0,0 +1,17 @@
+As of my last knowledge update in September 2021, Java (JDK) is indeed available for free for general use, but there are some nuances to be aware of. Let me break down the situation for you:
+
+1. **OpenJDK**: This is an open-source implementation of the Java Platform, Standard Edition (Java SE). OpenJDK is widely used and is maintained by the open-source community. It's available for free and can be used without any licensing fees. Many Linux distributions include OpenJDK as the default Java development kit.
+
+2. **Oracle JDK**: Oracle JDK (Java Development Kit) is provided by Oracle Corporation. Historically, Oracle JDK used to be free for development and testing, but there were potential costs associated with using it in production. However, starting with Oracle JDK 11 (released in September 2018), Oracle changed its licensing model. Oracle JDK 11 and later versions are no longer provided for free for commercial use in production environments. They require a subscription or a commercial license.
+
+3. **Oracle OpenJDK builds**: Oracle also provides builds of OpenJDK that are similar to Oracle JDK in terms of features, but these are based on the OpenJDK codebase. These builds can be used freely in development and production environments without any licensing fees.
+
+4. **AdoptOpenJDK and Other Distributors**: There are also other organizations and vendors, like AdoptOpenJDK (now part of the Eclipse Adoptium project), Amazon Corretto, and others, that provide JDK distributions based on the OpenJDK codebase. These distributions are typically free to use and may offer long-term support options.
+
+Given this information, if you're looking for a JDK to use, you generally have these options:
+
+- Use OpenJDK: This is a solid choice for most cases and is available for free.
+- Use Oracle OpenJDK builds: These are also free and offer features similar to Oracle JDK.
+- Explore alternative distributions like AdoptOpenJDK or Amazon Corretto: These are also free and may provide additional benefits.
+
+However, please note that the situation might have changed since my last update in September 2021. It's always a good idea to check the latest information on the official websites of Oracle, OpenJDK, and other JDK distributors to ensure you have the most up-to-date information regarding licensing and usage terms.
