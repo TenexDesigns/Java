f4b21578abf4c43140b0cffb2bc07b56427b62f6
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-14 15:31:04 +0300

    Create Concerns_Over_Oracle.c

---
+++ Java_and_Oracle/Concerns_Over_Oracle.c
@@ -0,0 +1,19 @@
+I apologize if my previous response was not clear. Let me clarify:
+
+In the past, there have been concerns and disagreements within the Java community due to changes in Oracles licensing policies and legal disputes. Here are a few examples:
+
+1. **Oracle vs. Google Lawsuit:** Oracle sued Google over the use of Java APIs in the Android operating system. This legal battle raised concerns about the potential implications of using Java and its APIs in a commercial context.
+
+2. **Licensing Changes:** Oracles changes in licensing terms for its JDK, especially after the release of Java 11, caused uncertainty among developers and organizations. The shift from the previous licensing model to a subscription-based one for certain commercial uses raised questions and sometimes confusion about the costs and requirements for using Oracle's JDK.
+
+While Oracle, as the current steward of the Java platform, has the ability to influence its direction and licensing, they cant unilaterally change the licensing of the Java programming language itself. The core Java programming language is governed by the Java Community Process (JCP), which involves multiple stakeholders, including Oracle, other companies, and the broader Java community. Changes to the Java language and its standard libraries typically require consensus and agreement within this community.
+
+The "Java" term encompasses both the programming language and the broader platform, including libraries, APIs, specifications, and related technologies. While Oracle has a significant role in the development and maintenance of the platform, it operates within the framework of the JCP, which aims to ensure that the platform remains open and benefits the wider developer community.
+
+So, while Oracle can influence the licensing and direction of its specific JDK distribution, it cant unilaterally change the fundamental licensing of the Java programming language itself without the cooperation and agreement of the Java community through the JCP process. This helps maintain the openness and stability of the Java ecosystem.
+
+
+
+
+
+..
