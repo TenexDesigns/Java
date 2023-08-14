9e6b6d23b54cd4204530637e5673fc8639ccbc61
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-14 15:29:55 +0300

    Create Open_JDK.c

---
+++ Java_and_Oracle/Open_JDK.c
@@ -0,0 +1,34 @@
+**OpenJDK** and the Oracle JDK are both implementations of the Java Platform, Standard Edition (Java SE). Here's an explanation of their differences in terms of licensing and usage:
+
+1. **OpenJDK:**
+   - OpenJDK (Open Java Development Kit) is an open-source implementation of the Java SE platform. It is developed as a collaborative project by the Java community under the governance of the OpenJDK project.
+   - OpenJDK provides the reference implementation of the Java SE specifications. It is licensed under the GNU General Public License, version 2, with the Classpath Exception (GPLv2+CE). This license allows you to use, modify, and distribute the software freely, even for commercial purposes.
+
+2. **Oracle JDK:**
+   - The Oracle JDK (Java Development Kit) is another implementation of the Java SE platform, provided by Oracle Corporation.
+   - In the past, Oracle JDK was offered under a dual licensing model: Oracle Binary Code License (BCL) for commercial use and the Oracle Technology Network (OTN) License for personal, development, and testing purposes. This meant that for certain commercial uses, a subscription or license was required.
+
+**Licensing Changes and Impact:**
+In recent years, there have been changes in how Oracle licenses its JDK. As of Java 11, Oracle JDK is available under the Oracle Technology Network License Agreement, which allows free use for personal, development, testing, prototyping, and demonstrating purposes, but requires a subscription or license for production use.
+
+**Implications for Users:**
+- **OpenJDK:** Being open source, OpenJDK is free to use for any purpose, including commercial usage. It's a popular choice for many organizations that want to avoid licensing costs and prefer an open-source solution.
+
+- **Oracle JDK:** While Oracle JDK is still free for personal, development, and testing purposes, commercial use requires a subscription or a commercial license. Organizations that require Oracle JDK for production use are encouraged to consider purchasing an Oracle subscription or exploring alternatives.
+
+**Java Language Freedom:**
+The Java programming language itself is not the subject of licensing fees. You can develop applications using the Java language without any cost implications. It's the implementation of the Java SE platform (like Oracle JDK) and associated tools that might involve licensing considerations.
+
+**When Java Might Not Be Free:**
+Java might not be free when:
+- You use an Oracle JDK version for production use without an appropriate subscription.
+- You use Oracle's commercial features (such as Java Flight Recorder and Java Mission Control) without the required licensing.
+
+**Actions with Free Java:**
+With OpenJDK or the free usage of Oracle JDK, you can:
+- Develop, test, and prototype applications without any licensing cost.
+- Distribute your applications along with the OpenJDK runtime without licensing fees.
+- Contribute to the OpenJDK project and help improve the Java platform.
+
+It's important to stay updated on the licensing terms and conditions, especially if you're using Java for commercial purposes. Always refer to the official Oracle and OpenJDK websites for the latest information on licensing and usage policies.
+
