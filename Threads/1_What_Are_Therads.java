48c73da652494104b416d8150be2f6ecf7e8ed9a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-19 10:00:00 +0300

    Update 1_What_Are_Therads.java

--- Threads/1_What_Are_Therads.java
+++ Threads/1_What_Are_Therads.java
@@ -62,4 +62,120 @@ Java threads have priorities ranging from 1 (lowest) to 10 (highest). However, t
 In summary, multithreading in Java allows you to execute multiple tasks concurrently, improving performance and responsiveness. While threads provide powerful capabilities, they also introduce challenges related to synchronization and resource management. Careful design, synchronization, and proper use of concurrency utilities are essential for writing robust multithreaded programs.
 
 
+
+
+
+
+MORE EXPLAANTION
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+# Introduction to Java Threads and Multithreading
+
+Java threads allow for concurrent execution of two or more parts of a program, which can lead to maximum utilization of the CPU. Each part of such a program is called a thread, and each thread defines a separate path of execution[^0^][^1^][^2^][^4^][^8^][^10^].
+
+## Creating Threads in Java
+
+There are two ways to create a thread in Java[^1^][^4^][^8^][^10^]:
+
+1. **Extending the Thread class**: We create a class that extends the `java.lang.Thread` class. This class overrides the `run()` method available in the Thread class. A thread begins its life inside `run()` method. We create an object of our new class and call `start()` method to start the execution of a thread[^0^].
+
+```java
+class MyThread extends Thread {
+    public void run(){
+        // code for the thread to execute
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        MyThread t = new MyThread();
+        t.start();
+    }
+}
+```
+
+2. **Implementing the Runnable interface**: We create a class that implements the `java.lang.Runnable` interface and override the `run()` method. Then, we create a Thread object, passing the instance of our class that implements Runnable to the Thread constructor. Finally, we start the thread by calling `start()` method[^1^][^8^].
+
+```java
+class MyRunnable implements Runnable {
+    public void run(){
+        // code for the thread to execute
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+    }
+}
+```
+
+## Thread Synchronization
+
+When multiple threads need to access shared resources, synchronization is used to ensure that only one thread can access the resource at a time[^18^]. Java provides synchronized methods and blocks to implement synchronized behavior[^18^]. For example:
+
+```java
+synchronized(object) {
+    // Block of statements to be synchronized
+}
+```
+
+## Common Problems in Multithreading
+
+1. **Race Condition**: A race condition occurs when two or more threads can access shared data and they try to change it at the same time[^13^]. This can lead to inconsistent results depending on the relative timing of the threads' execution[^13^].
+
+2. **Deadlock**: Deadlock is a situation where two or more threads are blocked forever, waiting for each other to release resources[^13^].
+
+3. **Starvation**: Starvation describes a situation where a thread is unable to gain regular access to shared resources and is unable to make progress[^13^].
+
+4. **Thread interference**: Interference happens when two operations, running in different threads, but acting on the same data, interleave. This means that the two operations consist of multiple steps, and the sequences of steps overlap[^13^].
+
+## Overcoming These Problems
+
+1. **Synchronization**: The Java keyword `synchronized` can be used to ensure that only one thread at a time can access a specific section of code[^13^][^18^].
+
+2. **Volatile Keyword**: The `volatile` keyword in Java is used as an indicator to the Java compiler to always read the value of a volatile variable from the main memory and not from the thread's local cache[^20^].
+
+3. **Atomic Classes**: Java provides atomic classes like `AtomicInteger`, `AtomicLong`, etc., which provide methods that are atomic. The atomic classes internally use compare-and-swap (CAS) instruction to achieve atomicity[^13^].
+
+4. **Lock Interface**: The `java.util.concurrent.locks` package contains several lock implementations, so you might not have to implement your own locks. The `ReentrantLock` class is a mutual exclusion lock with the same basic behavior as the implicit monitors accessed via the `synchronized` keyword but with extended capabilities[^13^].
+
+5. **Thread-safe Collections**: Java provides thread-safe collection classes like `CopyOnWriteArrayList`, `ConcurrentHashMap`, etc., that can be accessed simultaneously by multiple threads[^13^].
+
+```java
+CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+Map<String, String> map = new ConcurrentHashMap<>();
+```
+
+Remember, multithreading is a complex topic and requires a good understanding of the Java language and its libraries. It's also important to be aware of the potential issues that can arise when using threads, as well as the techniques available to manage these issues[^0^][^1^][^2^][^4^][^8^][^10^][^13^][^18^][^20^].
+
+Sources: [^0^](https://www.geeksforgeeks.org/multithreading-in-java/), [^1^](https://www.freecodecamp.org/news/how-to-get-started
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
   
