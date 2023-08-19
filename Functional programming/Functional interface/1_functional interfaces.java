806e2166892bf5495468552c8646b446fd4af53e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-19 11:36:41 +0300

    Update 1_functional interfaces.java

--- Functional programming/Functional interface/1_functional interfaces.java
+++ Functional programming/Functional interface/1_functional interfaces.java
@@ -29,7 +29,83 @@ In this example, the lambda expression `(x, y) -> x + y` implements the `apply` 
 
 Functional interfaces provide a foundation for many Java features, including the Streams API, method references, and more. They allow you to write expressive and concise code by passing behavior as arguments and returning behavior from methods, enabling functional programming paradigms in Java.
 
-  
+
+
+
+    HERE ARE EXAMPLES OF FUNCTIONAL INTERFACES
+
+
+    Certainly, here are examples of some commonly used functional interfaces in Java along with their usage:
+
+1. **`Function<T, R>`**: Represents a function that takes an argument of type `T` and produces a result of type `R`.
+
+```java
+import java.util.function.Function;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+        Function<Integer, String> intToString = n -> "Number: " + n;
+        String result = intToString.apply(42);
+        System.out.println(result); // Outputs: Number: 42
+    }
+}
+```
+
+2. **`Predicate<T>`**: Represents a function that takes an argument of type `T` and returns a boolean result.
+
+```java
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        System.out.println(isEven.test(4)); // Outputs: true
+        System.out.println(isEven.test(5)); // Outputs: false
+    }
+}
+```
+
+3. **`Consumer<T>`**: Represents an operation that takes an argument of type `T` and returns no result (void).
+
+```java
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        Consumer<String> printer = message -> System.out.println("Printing: " + message);
+        printer.accept("Hello, World!"); // Outputs: Printing: Hello, World!
+    }
+}
+```
+
+4. **`Supplier<T>`**: Represents a supplier of results, which doesn't take any arguments but produces a value of type `T`.
+
+```java
+import java.util.function.Supplier;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<Double> randomNumber = () -> Math.random();
+        System.out.println(randomNumber.get()); // Outputs a random number
+    }
+}
+```
+
+5. **`UnaryOperator<T>`**: Represents an operation on a single operand of type `T`, producing a result of the same type `T`.
+
+```java
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorExample {
+    public static void main(String[] args) {
+        UnaryOperator<Integer> square = n -> n * n;
+        int result = square.apply(5);
+        System.out.println(result); // Outputs: 25
+    }
+}
+```
+
+These are just a few examples of the many functional interfaces available in Java's `java.util.function` package. Each of these interfaces allows you to express behavior in a functional and concise way, making your code more readable and expressive.
 
 
 
