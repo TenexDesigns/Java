39689f605c4c0e7d02e5775c6840bcdd38a99da5
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-19 10:02:49 +0300

    Update 2_MultiThreading.java

--- Threads/2_MultiThreading.java
+++ Threads/2_MultiThreading.java
@@ -63,4 +63,111 @@ In summary, multithreading in Java allows you to harness the power of concurrenc
 
 
 
+
+
+
+
+
+
+MORE EXPLANANTION
+-----------------------------------------------------------------------------------------------------------------------------
+
+
+   # Introduction to Multithreading in Java
+
+Multithreading in Java is a feature that allows concurrent execution of two or more parts of a program for maximum utilization of CPU[^1^][^3^][^5^][^8^]. Each part of such a program is called a thread[^1^][^3^][^5^][^8^]. Threads are lightweight processes within a process[^1^][^5^].
+
+## Creating Threads in Java
+
+There are two ways to create a thread in Java[^1^][^2^][^4^][^8^]:
+
+1. **Extending the Thread class**: We create a class that extends the `java.lang.Thread` class. This class overrides the `run()` method available in the Thread class. A thread begins its life inside `run()` method. We create an object of our new class and call `start()` method to start the execution of a thread[^1^][^8^].
+
+```java
+class MyThread extends Thread {
+    public void run(){
+        // code for the thread to execute
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        MyThread t = new MyThread();
+        t.start();
+    }
+}
+```
+
+2. **Implementing the Runnable interface**: We create a class that implements the `java.lang.Runnable` interface and override the `run()` method. Then, we create a Thread object, passing the instance of our class that implements Runnable to the Thread constructor. Finally, we start the thread by calling `start()` method[^1^][^8^].
+
+```java
+class MyRunnable implements Runnable {
+    public void run(){
+        // code for the thread to execute
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+    }
+}
+```
+
+## Lifecycle of a Thread
+
+A thread goes through various stages in its lifecycle[^6^]. The stages include:
+
+- **New**: The thread is in this state after an instance of the Thread class is created but before the `start()` method is invoked on it[^6^].
+- **Runnable**: The thread is in this state after the `start()` method has been invoked on it[^6^].
+- **Running**: The thread is in this state when the JVM has selected it for execution[^6^].
+- **Waiting**: The thread is in this state when it waits for another thread to perform a task[^6^].
+- **Dead**: The thread is in this state when its `run()` method completes[^6^].
+
+## Synchronization in Multithreading
+
+Synchronization in multithreading is the capability to control the access of multiple threads to shared resources[^2^]. Without synchronization, it is possible for one thread to modify a shared object while another thread is in the process of using or updating that object's value. This often leads to significant errors[^2^].
+
+Java provides synchronized methods and blocks to implement synchronized behavior[^2^]. For example:
+
+```java
+synchronized(object) {
+    // Block of statements to be synchronized
+}
+```
+
+## Advantages of Multithreading
+
+1. **Utilization of CPU**: Multithreading allows maximum utilization of the CPU by multitasking[^1^][^3^][^5^][^8^].
+2. **Improved Performance**: Applications that are capable of doing many tasks at the same time can be built with multithreading[^5^].
+3. **Simplified Modelling**: Modeling the real world in terms of concurrent activities can be simplified with multithreading[^5^].
+
+## Disadvantages of Multithreading
+
+1. **Overhead in Communication**: Multithreading has overheads like communication between threads, switching between threads, and maintaining multiple threads[^5^].
+2. **Synchronization Issues**: Multithreading can lead to synchronization problems, deadlocks, and race conditions[^5^].
+
+In conclusion, Multithreading is a very important aspect of Java as it enables the concurrent execution of two or more parts of a program for maximum utilization of CPU[^1^][^3^][^5^][^8^].
+
+Sources: [^1^](https://www.geeksforgeeks.org/multithreading-in-java/), [^2^](https://www.developer.com/java/java-multithreading-explained/), [^3^](https://www.developer.com/java/multithreading-in-java/), [^4^](https://www.geeksforgeeks.org/java-multithreading-tutorial/), [^5^](https://www.javatpoint.com/multithreading-in-java), [^6^](https://www.guru99.com/multithreading-java.html), [^8^](https://ld101.hashnode.dev/multithreading-in-java-a-detailed-explanation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
   
