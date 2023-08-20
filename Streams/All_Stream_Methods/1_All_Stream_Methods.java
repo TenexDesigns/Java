4ca3df50e5af0421c1f009e308cd4adbfe227f6c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-20 17:52:16 +0300

    Create 1_All_Stream_Methods.java

---
+++ Streams/All_Stream_Methods/1_All_Stream_Methods.java
@@ -0,0 +1,133 @@
+Certainly, I'll provide you with brief explanations and code samples for each of the Stream methods you've mentioned:
+
+1. **`allMatch(Predicate<? super T> predicate)`**: Returns whether all elements of the stream match the provided predicate.
+   ```java
+   boolean allMatch = stream.allMatch(element -> element > 0);
+   ```
+
+2. **`anyMatch(Predicate<? super T> predicate)`**: Returns whether any element of the stream matches the provided predicate.
+   ```java
+   boolean anyMatch = stream.anyMatch(element -> element > 10);
+   ```
+
+3. **`Stream.Builder<T> builder()`**: Returns a builder for creating a Stream.
+   ```java
+   Stream.Builder<Integer> builder = Stream.builder();
+   builder.add(1).add(2).add(3);
+   Stream<Integer> builtStream = builder.build();
+   ```
+
+4. **`collect(Collector<? super T, A, R> collector)`**: Performs a mutable reduction operation on the stream elements using a Collector.
+   ```java
+   List<String> collectedList = stream.collect(Collectors.toList());
+   ```
+
+5. **`count()`**: Returns the count of elements in the stream.
+   ```java
+   long count = stream.count();
+   ```
+
+6. **`distinct()`**: Returns a stream of distinct elements.
+   ```java
+   Stream<Integer> distinctStream = stream.distinct();
+   ```
+
+7. **`filter(Predicate<? super T> predicate)`**: Returns a stream of elements that match the provided predicate.
+   ```java
+   Stream<Integer> filteredStream = stream.filter(element -> element > 5);
+   ```
+
+8. **`findFirst()`**: Returns an Optional describing the first element of the stream.
+   ```java
+   Optional<Integer> firstElement = stream.findFirst();
+   ```
+
+9. **`flatMap(Function<? super T, ? extends Stream<? extends R>> mapper)`**: Transforms each element into a stream of elements and then flattens them into a single stream.
+   ```java
+   Stream<String> flatMappedStream = stream.flatMap(str -> Stream.of(str.split("")));
+   ```
+
+10. **`forEach(Consumer<? super T> action)`**: Applies an action to each element of the stream.
+    ```java
+    stream.forEach(element -> System.out.println(element));
+    ```
+
+11. **`generate(Supplier<T> s)`**: Generates an infinite sequential unordered stream based on the provided Supplier.
+    ```java
+    Stream<Integer> generatedStream = Stream.generate(() -> new Random().nextInt());
+    ```
+
+12. **`limit(long maxSize)`**: Returns a stream with the first `maxSize` elements.
+    ```java
+    Stream<Integer> limitedStream = stream.limit(10);
+    ```
+
+13. **`map(Function<? super T, ? extends R> mapper)`**: Transforms each element using the provided function.
+    ```java
+    Stream<String> mappedStream = stream.map(String::toUpperCase);
+    ```
+
+14. **`max(Comparator<? super T> comparator)`**: Returns the maximum element according to the provided Comparator.
+    ```java
+    Optional<Integer> maxElement = stream.max(Comparator.naturalOrder());
+    ```
+
+15. **`min(Comparator<? super T> comparator)`**: Returns the minimum element according to the provided Comparator.
+    ```java
+    Optional<Integer> minElement = stream.min(Comparator.naturalOrder());
+    ```
+
+16. **`noneMatch(Predicate<? super T> predicate)`**: Returns whether no elements match the provided predicate.
+    ```java
+    boolean noneMatch = stream.noneMatch(element -> element < 0);
+    ```
+
+17. **`of(T... values)`**: Creates a sequential ordered stream from the provided values.
+    ```java
+    Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
+    ```
+
+18. **`peek(Consumer<? super T> action)`**: Performs an action on each element and returns the original stream.
+    ```java
+    Stream<Integer> peekedStream = stream.peek(element -> System.out.println(element));
+    ```
+
+19. **`reduce(BinaryOperator<T> accumulator)`**: Reduces the stream elements using the provided accumulator function.
+    ```java
+    Optional<Integer> reducedValue = stream.reduce((a, b) -> a + b);
+    ```
+
+20. **`skip(long n)`**: Returns a stream with the first `n` elements skipped.
+    ```java
+    Stream<Integer> skippedStream = stream.skip(5);
+    ```
+
+21. **`sorted()`**: Returns a stream with elements sorted according to natural order.
+    ```java
+    Stream<Integer> sortedStream = stream.sorted();
+    ```
+
+22. **`toArray()`**: Converts the stream to an array.
+    ```java
+    Object[] array = stream.toArray();
+    ```
+
+These code samples should help you understand how these Stream methods work and how to use them effectively in Java programming.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
