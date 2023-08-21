801e10f22c3412a0c4244eb60fac81d0552a34ee
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-21 09:43:18 +0300

    Create 1_Map_Vs_Flatmap.java

---
+++ Streams/Map_Vs_FlatMap/1_Map_Vs_Flatmap.java
@@ -0,0 +1,137 @@
+In Java streams, both `map` and `flatMap` are used for data transformation, but they serve different purposes based on the structure of the data and the desired outcome. Let's dive into their differences and use cases.
+
+**1. `map` Operation:**
+
+The `map` operation is used to transform each element of a stream into another element by applying a given function. It performs a one-to-one transformation. The resulting stream will have the same number of elements as the original stream.
+
+```java
+List<String> words = Arrays.asList("hello", "world");
+List<String> upperCaseWords = words.stream()
+                                   .map(String::toUpperCase)
+                                   .collect(Collectors.toList());
+// Result: ["HELLO", "WORLD"]
+```
+
+**2. `flatMap` Operation:**
+It is Used when you have a stream of collections or streams and you want to combine them into a single stream. e.g [[1,2,3],[4,5,6]] then use flatmap to convet it to one stream -> [1,2,3,4,5,6]
+Remembert to use the second .stream method to flatten the array or stream of streams
+  
+The `flatMap` operation is used when each element of a stream is transformed into multiple elements, possibly from a nested structure (like a nested list or another stream). It performs a one-to-many transformation and "flattens" the results into a single stream.
+
+```java
+List<List<Integer>> nestedList = Arrays.asList(
+    Arrays.asList(1, 2, 3),
+    Arrays.asList(4, 5, 6),
+    Arrays.asList(7, 8, 9)
+);
+List<Integer> flattenedList = nestedList.stream()
+                                        .flatMap(nums -> nums.stream())  // This is the second stream method to flatten the array of strams to one steam
+                                        .collect(Collectors.toList());
+// Result: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+```
+
+**Use Cases:**
+
+- **Use `map` when:** You want to perform a one-to-one transformation of elements. For example, converting objects of one type to another or applying some operation to each element.
+
+- **Use `flatMap` when:** You want to transform each element into multiple elements or when dealing with nested structures. It's often used when you have a stream of collections or streams and you want to combine them into a single stream.
+
+**Key Differences:**
+
+- `map` transforms each input element into a single output element, maintaining a one-to-one relationship.
+- `flatMap` transforms each input element into a stream of output elements, which are then combined into a single stream.
+- `map` is used for simple transformations and maintains the structure of the original stream.
+- `flatMap` is used for complex transformations that involve flattening nested structures.
+
+**Example:**
+
+Consider you have a list of sentences, and you want to extract all unique words from these sentences. You can use `flatMap` to achieve this:
+
+```java
+List<String> sentences = Arrays.asList("Hello world", "Java streams are powerful", "Hello again");
+List<String> uniqueWords = sentences.stream()
+                                    .map(sentence -> sentence.split(" "))
+                                    .flatMap(Arrays::stream)
+                                    .distinct()
+                                    .collect(Collectors.toList());
+// Result: ["Hello", "world", "Java", "streams", "are", "powerful", "again"]
+```
+
+In this example, `map` is used to split each sentence into an array of words, and then `flatMap` is used to flatten these arrays into a single stream of words. Finally, the `distinct` operation ensures only unique words are collected.
+
+
+
+
+
+
+
+
+MORE EXPLAANNTION
+----------------------------------------------------------------------------------------------------------------------------
+
+  In Java, `map()` and `flatMap()` are intermediate operations in the Stream interface, used for transformation and mapping operations. Both methods return another stream as their output, but they differ in how they process and return this output [Source 1](https://www.geeksforgeeks.org/difference-between-map-and-flatmap-in-java-stream/).
+
+### map() in Java Stream
+
+The `map()` method produces one output for each input value, making it suitable for one-to-one mapping. This method takes a Function, applies it to each value in the input stream, and produces one result value that is sent to the output stream. It's used where you need to map the elements of a particular collection to a certain function, and then return a stream containing the updated results [Source 2](https://stackoverflow.com/questions/26684562/whats-the-difference-between-map-and-flatmap-methods-in-java-8). 
+
+Here's an example of using `map()` to multiply all the elements of a list by 3:
+
+```java
+List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+List<Integer> tripled = numbers.stream()
+                               .map(n -> n * 3)
+                               .collect(Collectors.toList());
+```
+
+### flatMap() in Java Stream
+
+Unlike `map()`, `flatMap()` can produce an arbitrary number of values (zero or more) for each input value. This makes it suitable for one-to-many mapping. The `flatMap()` operation takes a function that conceptually wants to consume one value and produce an arbitrary number of values. It's used where you need to flatten or transform a string, as you cannot flatten a string using `map()` [Source 1](https://www.geeksforgeeks.org/difference-between-map-and-flatmap-in-java-stream/).
+
+Here's an example of using `flatMap()` to get the first character of all the strings present in a list of strings and returning the result in the form of a stream:
+
+```java
+List<String> words = Arrays.asList("Hello", "World");
+List<String> characters = words.stream()
+                               .flatMap(str -> str.chars().mapToObj(c -> (char) c))
+                               .map(c -> String.valueOf(c))
+                               .collect(Collectors.toList());
+```
+
+In this example, `flatMap()` is applied to each string in the list, converting each string into individual characters, and then flattening all the characters into a single stream [Source 3](https://stackabuse.com/java-8-difference-between-map-and-flatmap/).
+
+### When to Use map() and flatMap()
+
+In summary, use `map()` when each input element maps to a single output element. Use `flatMap()` when each input element can map to multiple output elements (or none at all).
+
+Additionally, `flatMap()` is useful when dealing with nested containers (like `Stream<Stream<T>>` or `Optional<Optional<T>>`). It can flatten the nested layers into a single layer, making subsequent operations on the stream or optional more straightforward [Source 2](https://stackoverflow.com/questions/26684562/whats-the-difference-between-map-and-flatmap-methods-in-java-8).
+
+Always remember to choose the operation that best suits your use case to make your code more readable and maintainable.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
