560864821ffa909be8cbf4cf969e7e605d17f53b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-23 15:31:36 +0300

    Update ReadWriteLock and ReentrantLock.java

--- Threads/ConcurrentPackage/ReentrantLock/ReadWriteLock and ReentrantLock.java
+++ Threads/ConcurrentPackage/ReentrantLock/ReadWriteLock and ReentrantLock.java
@@ -40,4 +40,58 @@ public class ReadWriteLockExample {
 
 
 
-  
+  -----------------------------------------------------------------------------------------------------------------------
+
+
+   Both `ReadWriteLock` and `ReentrantLock` are concurrency constructs provided by the `java.util.concurrent.locks` package in Java, but they serve different purposes and are used in different scenarios. Let's explore the differences between `ReadWriteLock` and `ReentrantLock`:
+
+1. **Purpose:**
+
+   - **ReentrantLock:**
+     `ReentrantLock` is a general-purpose lock that allows exclusive access to a resource by one thread at a time. It supports reentrant locking, meaning a thread that holds the lock can reacquire it without blocking itself.
+
+   - **ReadWriteLock:**
+     `ReadWriteLock` is designed for scenarios where multiple threads can read a shared resource concurrently, but only one thread can write to it at a time. It provides two types of locks: read lock and write lock.
+
+2. **Locking Modes:**
+
+   - **ReentrantLock:**
+     `ReentrantLock` provides exclusive locking, meaning only one thread can hold the lock at a time. It's suitable when you want to ensure that a critical section of code is executed by only one thread at a time.
+
+   - **ReadWriteLock:**
+     `ReadWriteLock` provides two types of locks:
+     - Read Lock: Allows multiple threads to acquire the lock for reading simultaneously, as long as no thread holds the write lock.
+     - Write Lock: Exclusive lock that allows only one thread to acquire the lock for writing, and no other thread (read or write) can acquire it concurrently.
+
+3. **Use Cases:**
+
+   - **ReentrantLock:**
+     Use `ReentrantLock` when you need to control exclusive access to a shared resource, and reentrant behavior is necessary. It's appropriate for situations where you want to prevent multiple threads from simultaneously modifying a resource.
+
+   - **ReadWriteLock:**
+     Use `ReadWriteLock` when you have a resource that is frequently read but infrequently written. This allows multiple threads to read concurrently, improving throughput, while still ensuring exclusive access when writing.
+
+4. **Performance:**
+
+   - **ReentrantLock:**
+     Since `ReentrantLock` provides exclusive access, it might lead to contention and potentially lower performance in scenarios where many threads compete for the lock.
+
+   - **ReadWriteLock:**
+     `ReadWriteLock` can improve performance in scenarios where read operations dominate. It allows multiple threads to read simultaneously, reducing contention and improving throughput.
+
+5. **Lock Hierarchy:**
+
+   - **ReentrantLock:**
+     Supports reentrant locking, meaning a thread can reacquire the lock it already holds. This is useful for preventing deadlocks when a thread needs to call a method that also uses the lock.
+
+   - **ReadWriteLock:**
+     Does not support reentrant write locks. A thread that holds a write lock cannot reacquire it. Read locks, however, are reentrant, meaning a thread that holds a read lock can acquire additional read locks.
+
+In summary, use `ReentrantLock` when you need exclusive access to a shared resource with reentrant behavior. Use `ReadWriteLock` when you have a shared resource that is predominantly read, and you want to allow multiple threads to read concurrently while still ensuring exclusive access for write operations. Each of these constructs addresses different concurrency scenarios and should be chosen based on the specific requirements of your application.
+
+
+
+
+
+
+   
