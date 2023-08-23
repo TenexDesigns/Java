d9467cc7859b87a198fd82cb4349710117d17f2e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-23 10:33:47 +0300

    Create 1_Runnable and Callable.java

---
+++ Threads/ConcurrentPackage/Runnable and Callable/1_Runnable and Callable.java
@@ -0,0 +1,87 @@
+`Callable` and `Runnable` are both interfaces in Java that represent tasks to be executed concurrently, often in the context of thread pools, concurrency, and multithreading. They serve slightly different purposes, and the choice between them depends on whether your task needs to return a result or not.
+
+1. **Runnable:**
+   - `Runnable` is a functional interface with a single `run()` method that doesn't return a result.
+   - It's used to represent tasks that perform actions but don't produce a result.
+
+```java
+public interface Runnable {
+    void run();
+}
+```
+
+Here's a simple example of using `Runnable`:
+
+```java
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        Runnable task = () -> {
+            System.out.println("Task executed by " + Thread.currentThread().getName());
+        };
+
+        executor.submit(task);
+
+        executor.shutdown();
+    }
+}
+```
+
+2. **Callable:**
+   - `Callable` is a functional interface with a single `call()` method that returns a result and can throw an exception.
+   - It's used to represent tasks that produce a result and may need to handle checked exceptions.
+
+```java
+import java.util.concurrent.Callable;
+
+public interface Callable<V> {
+    V call() throws Exception;
+}
+```
+
+Here's a simple example of using `Callable`:
+
+```java
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableExample {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        Callable<Integer> task = () -> {
+            return 42;
+        };
+
+        Future<Integer> future = executor.submit(task);
+
+        try {
+            Integer result = future.get();
+            System.out.println("Result: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        executor.shutdown();
+    }
+}
+```
+
+**When to Use Which:**
+- Use `Runnable` when you need to execute a task that performs actions and doesn't return a result.
+- Use `Callable` when you need to execute a task that produces a result and may throw checked exceptions.
+- In thread pools and concurrent environments, both `Runnable` and `Callable` can be submitted to an `ExecutorService`. You can use `Future` to retrieve results and handle exceptions.
+
+In summary, choose `Runnable` for tasks without results and `Callable` for tasks with results or that need to handle checked exceptions. The appropriate choice depends on the nature of the task you want to execute concurrently.
+
+
+
+
+
+
+  
